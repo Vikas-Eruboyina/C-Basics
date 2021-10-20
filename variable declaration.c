86f639231e7169198d9126babfe9c9f1514d1927
Nameisvikas <89003961+Nameisvikas@users.noreply.github.com>
2021-10-20 10:56:59 +0530

    Add files via upload

---
+++ variable declaration.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	//Variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	
+	//intialisation
+	age=17;
+	cgpa=8.6;
+	initial='B';
+	
+	
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c",initial);
+}
